e3e540e93cd648381838e5161180016d280540b6
Nachiket Patel <patel.nachiket.r@gmail.com>
2019-12-21 16:14:02 +0530

    more functions

---
+++ text-case/handler.js
@@ -0,0 +1,36 @@
+"use strict"
+
+module.exports = async (event, context) => {
+    let err;
+    const result =             {
+        status: "Received input: " + JSON.stringify(event.body)
+    };
+
+    return context
+        .status(200)
+        .succeed(result);
+}
+
+
+
+const { ApolloServer, gql } = require('apollo-server');
+
+// A schema is a collection of type definitions (hence "typeDefs")
+// that together define the "shape" of queries that are executed against
+// your data.
+const typeDefs = gql`
+  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
+
+  # This "Book" type defines the queryable fields for every book in our data source.
+  type Book {
+    title: String
+    author: String
+  }
+
+  # The "Query" type is special: it lists all of the available queries that
+  # clients can execute, along with the return type for each. In this
+  # case, the "books" query returns an array of zero or more Books (defined above).
+  type Query {
+    books: [Book]
+  }
+`;
